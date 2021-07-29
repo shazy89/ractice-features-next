bf027a2b3c078b6195d3f6c7527b239b9d4aa3c8
shazy89 <erdo.shazy123@gmail.com>
2021-07-29 18:26:43 +0200

    fetching the data

---
+++ helpers/api-utils.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://practice-next-250c4-default-rtdb.firebaseio.com",
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+export async function getAllEvents() {
+  const res = await api.get("/events.json");
+  // const data = await response.json();
+
+  const events = [];
+  for (const key in res.data) {
+    events.push({
+      id: key,
+      ...res.data[key] // we can use the spred operator for dynamic display of the data
+    });
+  }
+  console.log(events);
+  return events;
+}
