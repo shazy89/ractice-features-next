9609a662cf12fdfaa788cd7d362333457dba6a52
shazy89 <erdo.shazy123@gmail.com>
2021-07-30 08:36:37 +0200

    implementing the react spring

--- pages/parallax/index.js
+++ pages/parallax/index.js
@@ -1,4 +1,4 @@
-import { Parallax } from "react-scroll-parallax";
+import { useSpring, animated, to } from "react-spring";
 import Image from "next/image";
 import face from "../../public/computer-icons-user-profile-person-thumbnail_bxdnq2.jpg";
 import styles from "../../styles/loading.module.css";
@@ -7,44 +7,7 @@ import Link from "next/link";
 const ParalaxImage = () => {
   return (
     <>
-      <section className={styles.space}>
-        <Link href="/">
-          <a>Homepage</a>
-        </Link>
-
-        <div className="content">
-          <Parallax offsetYMin={800} offsetYMax={1500}>
-            <h2>Moving A</h2>
-          </Parallax>
-          <Parallax offsetYMin={-500} offsetYMax={2000}>
-            <h2>Moving C</h2>
-          </Parallax>
-        </div>
-
-        <style jsx>
-          {`
-            .content {
-              margin-top: 300px;
-              border: 2px solid grey;
-              height: 1500px;
-            }
-          `}
-        </style>
-      </section>
-      {
-        <Parallax
-          className={styles.parallax__image}
-          y={[-30, 20]}
-          tagOuter="figure"
-        >
-          <Image
-            alt="Picture of the author"
-            width={500}
-            height={500}
-            src={face}
-          />
-        </Parallax>
-      }
+      <h1>HEy</h1>
     </>
   );
 };
---
+++ pages/spring/index.js
@@ -0,0 +1,5 @@
+const Spring = () => {
+  return <div></div>;
+};
+
+export default Spring;
