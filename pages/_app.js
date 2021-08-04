a5fee37a083bb6b6124c286fb8cf003230443e6a
shazy89 <erdo.shazy123@gmail.com>
2021-08-04 17:39:41 +0200

    global variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import "../styles/globals.css";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
