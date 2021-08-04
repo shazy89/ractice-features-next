180037af641ef4c31a60b512407a27e9065de5a6
shazy89 <erdo.shazy123@gmail.com>
2021-08-04 20:17:51 +0200

    export the styles for the card

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,27 @@
 import { getAllEvents } from "../helpers/api-utils";
 import Button from "@material-ui/core/Button";
 import MyButton from "../components/buttons/Button";
-
+import MyCard from "../components/cards/MyCard";
 export default function Home({ events }) {
   console.log(events);
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      <MyButton color="black" text="white" width={12}>
-        Hey There
-      </MyButton>
-    </div>
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <MyButton color="black" text="white" width={12}>
+          Hey There
+        </MyButton>
+        <MyButton color="black" text="white" width={22}>
+          Hey There
+        </MyButton>
+      </div>
+      <div>
+        <MyCard />
+      </div>
+    </>
   );
 }
 export async function getStaticProps() {
