79d7c5ed61d1ec2bd240a2971782f5a131c14728
shazy89 <erdo.shazy123@gmail.com>
2021-08-06 08:58:15 +0200

    testing the scroll event

--- components/Sroll.js
+++ components/Sroll.js
@@ -1,7 +1,24 @@
-import navbar from "../styles/navbar.module.css";
+import { useState, useEffect } from "react";
+import navbar from "../styles/scroll.module.css";
 
 const Sroll = ({ children }) => {
-  return <div className={navbar.scroll}>{children}</div>;
+  const [scrollHeight, stScrollHeight] = useState(0);
+  const onScroll = (e) => {
+    //   stScrollHeight(e.srcElement);
+    let element = e.target;
+    console.log(element.scrollHeight);
+    console.log(element.scrollTop);
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      // do something at end of scroll
+      console.log(stScrollHeight(element.scrollHeight - element.scrollTop));
+    }
+  };
+  console.log(scrollHeight);
+  return (
+    <div className={navbar.scroll} onScroll={onScroll}>
+      {children}
+    </div>
+  );
 };
 
 export default Sroll;
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,10 @@ export default function Home({ events }) {
   return (
     <>
       <Navbar />
-      <Structure />
+      <Scroll>
+        <Structure />
+        <Structure />
+      </Scroll>
     </>
   );
 }
